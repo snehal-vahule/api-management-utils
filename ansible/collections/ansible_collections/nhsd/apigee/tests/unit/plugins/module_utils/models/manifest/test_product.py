c75b2cbd3de65df63ca39cf631e162fb95db85da
Alex Carrie <acarrie.dev@gmail.com>
2021-05-28 10:17:02 +0100

    APM-2211 Add test for manual approval product exceptions

--- ansible/collections/ansible_collections/nhsd/apigee/tests/unit/plugins/module_utils/models/manifest/test_product.py
+++ ansible/collections/ansible_collections/nhsd/apigee/tests/unit/plugins/module_utils/models/manifest/test_product.py
@@ -38,3 +38,37 @@ def test_prod_cannot_have_auto_approvalType_on_products(
     }
     product = ApigeeProduct(**raw_product)
     assert product.approvalType == final_approvalType
+
+@pytest.mark.parametrize(
+    "name,env,initial_approvalType,final_approvalType",
+    [
+        ("canary-api-prod", "prod", "auto", "auto"),
+        ("canary-api-prod", "prod", "manual", "manual"),
+        ("non-exception-product", "prod", "auto", "manual"),
+        ("non-exception-product", "prod", "manual", "manual"),
+    ],
+)
+def test_manual_approval_exception_list_on_prod(
+    name, env, initial_approvalType, final_approvalType
+):
+    raw_product = {
+        "name": name,
+        "approvalType": initial_approvalType,
+        "attributes": [
+            {"name": "access", "value": "public"},
+            {"name": "ratelimit", "value": "300pm"},
+        ],
+        "description": "testing our validators",
+        "displayName": "Test Product",
+        "environments": [env],
+        "proxies": [f"identity-service-{env}"],
+        "scopes": [
+            "urn:nhsd:apim:app:level3:test-service",
+            "urn:nhsd:apim:user-nhs-login:P9:test-service",
+        ],
+        "quota": "300",
+        "quotaInterval": "1",
+        "quotaTimeUnit": "minute",
+    }
+    product = ApigeeProduct(**raw_product)
+    assert product.approvalType == final_approvalType
