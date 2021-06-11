1a51265ade92d84c7705b7f5a04442357df4d749
Jodie Barnsley <jodiebarnsley27@gmail.com>
2021-06-11 10:09:23 +0100

    APM-2241 make get_token cli arg

--- scripts/purge_incorrect_env_vars.py
+++ scripts/purge_incorrect_env_vars.py
@@ -6,7 +6,7 @@ purge_incorrect_env_vars.py
 Takes data from monitoring and value to be removed, and removes offending entires.
 
 Usage:
-  purge_incorrect_env_vars.py [-m <map_name> | --map_name=<map_name>] [-e <entry_name> | --entry_name=<entry_name>] [-v <value> | --value=<value>]
+  purge_incorrect_env_vars.py [-m <map_name> | --map_name=<map_name>] [-e <entry_name> | --entry_name=<entry_name>] [-v <value> | --value=<value>] [-t <token> | --token=<token>]
   purge_incorrect_env_vars.py (-h | --help)
 
 Options:
@@ -14,41 +14,37 @@ Options:
   -m <map_name> | --map_name=<map_name>         Name of Apigee key-value map to edit
   -e <entry_name> | --entry_name=<entry_name>   Name of KVM entry to edit
   -v <value> | --value=<value>                  String value to be removed
+  -t <token> | --token=<token>                  Auth token (get_token)
 """
-
 import json
 import requests
-import os
 from docopt import docopt
-
-TOKEN = os.getenv('TOKEN')
-
 
 def delete_offending_entires(json_data, offending_str):
 	for api in json_data:
 		for env in json_data[api]:
-			for line in json_data[api][env]:
-				if offending_str in line:
-					print(line)
+				for line in json_data[api][env]:
+					if offending_str in line:
+						print(line)        
 
 
 def main(args):
     input_value = str(args["--value"])
     map_name = str(args["--map_name"])
     entry_name = str(args["--entry_name"])
+    token = str(args["--token"])
 
     org_name = "nhsd-nonprod"
     env_name = "internal-dev"
     url = f"https://api.enterprise.apigee.com/v1/organizations/{org_name}/environments/{env_name}/keyvaluemaps/{map_name}/entries/{entry_name}"
-    auth = f"Bearer {TOKEN}"
+    auth = f"Bearer {token}"
     resp = requests.get(url, headers = {"Authorization": auth})
+
 
     json_data = json.loads(resp.text).get('value')
     json_data = json.loads(json_data)
 
-
     delete_offending_entires(json_data, input_value)
-
 
 
 if __name__ == "__main__":
